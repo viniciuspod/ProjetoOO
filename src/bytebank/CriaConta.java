28ce6e34d7b313c5387a186146547ac97526feca
viniciuspod <viniciuspodantas@gmail.com>
2021-10-18 15:26:04 -0300

    criando repositorio no git

---
+++ src/bytebank/CriaConta.java
@@ -0,0 +1,21 @@
+package bytebank;
+
+public class CriaConta {
+
+	
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		
+		
+		primeiraConta.saldo += 100;
+		System.out.println("primeira conta tem " + primeiraConta.saldo);
+		
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("segunda conta tem " + segundaConta.saldo);
+	}
+
+}
