a9b4a5bf721f5bf5becca7c0251ae6ebc5f87a03
viniciuspod <viniciuspodantas@gmail.com>
2021-10-19 16:14:48 -0300

    criando metodo transfere

--- src/bytebank/testaMetodo.java
+++ src/bytebank/testaMetodo.java
@@ -12,7 +12,17 @@ public class testaMetodo {
 		boolean conseguiuRetirar = conta.saca(20);
 		System.out.println(conta.saldo);
 		System.out.println(conseguiuRetirar);
-
+		
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		
+		contaDaMarcela.transfere(300, conta);
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(conta.saldo);
+			
+		
 	}
 
 }
